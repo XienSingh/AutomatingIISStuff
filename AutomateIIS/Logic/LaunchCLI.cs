be203630b2ec3f1e27dd03b76054eb774f388007
Xien Singh <xiensingh007@gmail.com>
2020-04-14 22:03:35 +0200

    changes for version 2

--- AutomateIIS/Logic/LaunchCLI.cs
+++ AutomateIIS/Logic/LaunchCLI.cs
@@ -7,50 +7,45 @@ using System.Web;
 
 namespace AutomateIIS.Logic
 {
-    public class LaunchCLI
-    {
-        public static string LaunchCommandLineApp(int SiteID, string[] bindings)
-        {
-            const string SSLType = "N";
-            var SiteIDforSSL = SiteID;
+	public class LaunchCLI
+	{
+		public static string LaunchCommandLineApp(int SiteID, string[] bindings)
+		{
 
-            // Use ProcessStartInfo class
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = false;
-            startInfo.UseShellExecute = true;
-            startInfo.FileName = @"C:\Users\Xien\Source\Repos\AutomatingIISStuff\win-acme-1.9.12.2\letsencrypt-win-simple\bin\Debug\letsencrypt.exe";
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.Verb = "runas";
-            startInfo.Arguments = SSLType + " " + SiteIDforSSL.ToString() + " " + string.Join(",", bindings);
+			// Use ProcessStartInfo class
+			ProcessStartInfo startInfo = new ProcessStartInfo();
+			startInfo.CreateNoWindow = false;
+			startInfo.UseShellExecute = true;
+			startInfo.FileName = @"C:\SSL\WACS\wacs.exe";
+			startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			startInfo.Verb = "runas";
+			startInfo.Arguments = $" --target manual --host {string.Join(",", bindings)} --siteid {SiteID}  --installation iis  --installationsiteid {SiteID}";
 
-            try
-            {
-                using (Process exeProcess = Process.Start(startInfo))
-                {
-                    exeProcess.WaitForExit();
-                    switch (exeProcess.ExitCode)
-                    {
-                        case 0:
-                            return "No action taken";
+			try
+			{
+				using (Process exeProcess = Process.Start(startInfo))
+				{
+					exeProcess.WaitForExit();
+					switch (exeProcess.ExitCode)
+					{
+						case 0:
+							return "Cert Added Successfully to : " + string.Join(",", bindings); ;
 
-                        case 1:
-                            return "Cert Added Successfully to : "+ string.Join(",", bindings); ;
+						case -1:
+							return "Exeption in adding Cert, Please contact the internal SSL Issuer";
 
-                        case 2:
-                            return "Exeption in adding Cert, Please contact the internal SSL Issuer";
+						default:
+							return "Somethng happend but I dont know what...";
 
-                        default:
-                            return "Somethng happend but I dont know what...";
+					}
+				}
 
-                    }
-                }
-
-            }
-            catch (Exception e)
-            {
-                //TODO: Log error.
-                return "Failed with errors";
-            }
-        }
-    }
+			}
+			catch (Exception e)
+			{
+				//TODO: Log error.
+				return "Failed with errors";
+			}
+		}
+	}
 }
